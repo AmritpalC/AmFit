378bb73d8b38cc742868e84692b9a13d2a1ca6e3
AmritpalC <amritpalchahal@hotmail.co.uk>
2023-06-01 01:45:10 +0100

    implement authorisation

--- routes/workouts.js
+++ routes/workouts.js
@@ -3,13 +3,21 @@ const router = express.Router();
 
 // Import Controllers
 const workoutsCtrl = require('../controllers/workouts');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 // GET /workouts
 router.get('/', workoutsCtrl.index);
+
 // GET /workouts/new
+// ? the ensureLoggedIn will call next provided the user is logged in, so that they can create a new workout
+// ? adding these routes for later - also add update functionality -> PUT /workouts/:idß
+// ! add same protection to exercises routes and comments (if I reach that)
+// router.get('/new', ensureLoggedIn, workoutsCtrl.new);
 
 // GET /workouts/:id (show functionality)- Must be below new route
+// router.get('/:id', workoutsCtrl.show);
 
 // POST /workouts
+// router.post('/', ensureLoggedIn, workoutsCtrl.create);
 
 module.exports = router;
