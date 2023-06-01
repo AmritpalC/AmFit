a74c2931f0dd5c6481af675a2a1aa35000981152
AmritpalC <amritpalchahal@hotmail.co.uk>
2023-06-01 12:20:27 +0100

    logo, workout data resource with schema and model, 2 test entries with mongosh

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,9 +1,17 @@
+// Importing model
+const Workout = require('../models/workout')
+
 // GET /workouts
 
-function index(req, res, next) {
+async function index(req, res, next) {
+    // want to query workouts collection and return all workouts, 
+    // passing them into the render template
+    const allWorkouts = await Workout.find({})
+    console.log(allWorkouts)
     res.render('workouts/index',
     {
-        title: 'Search Workouts'
+        title: 'Search Workouts',
+        workouts: allWorkouts
     })
 }
 
---
+++ models/workout.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// ! change exercises to array with type objectId and ref Exercise once set up
+// ! will need to add required validation to the exercises array to ensure it has >= 1 value
+// ? comments & likes will be a strectch goal, so not included for now
+
+const workoutSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    date: { type: Date, required: true },
+    category: { type: String, required: true },
+    duration: { type: Number, required: true },
+    calories: { type: Number },
+    exercises: { type: String, required: true },
+    // likes: { type: String },
+    // comments: { type: String }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Workout', workoutSchema)
